4bc5f47fe7c63dbc08fd44b1f2aa00062ea07699
lakhara <siddharth.lakhara@gmail.com>
2021-06-24 18:15:05 +0530

    add recipe list

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Roti',
+      'Tasty and round Roti',
+      'https://image.shutterstock.com/image-photo/indian-bread-roti-600w-1019185939.jpg'
+    ),
+    new Recipe(
+      'Paneer Butter Masala',
+      'Tasty paneer curry with creamy gravy',
+      'https://image.shutterstock.com/image-photo/paneer-butter-masala-cheese-cottage-600w-620764175.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
